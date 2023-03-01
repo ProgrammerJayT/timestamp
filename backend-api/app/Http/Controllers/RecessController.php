f3937880f9341453b1020c7a18aaddb5781f663b
ProgrammerJayT <theanthem8@gmail.com>
2023-03-01 23:18:44 +0200

    Created recess entity

---
+++ backend-api/app/Http/Controllers/RecessController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Recess;
+use App\Http\Requests\StoreRecessRequest;
+use App\Http\Requests\UpdateRecessRequest;
+
+class RecessController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRecessRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Recess $recess)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Recess $recess)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRecessRequest $request, Recess $recess)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Recess $recess)
+    {
+        //
+    }
+}
