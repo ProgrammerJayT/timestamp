1558ba90ce843faf7f97d318a304a72972b1798e
ProgrammerJayT <theanthem8@gmail.com>
2023-03-13 14:12:19 +0200

    Created student, lecturer, and admin relationship

--- backend-api/app/Models/Account.php
+++ backend-api/app/Models/Account.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Admin;
+use App\Models\Student;
+use App\Models\Lecturer;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Account extends Model
 {
+    protected $primaryKey = 'account_id';
+    protected $guarded = [];
     use HasFactory;
+
+    public function student()
+    {
+        return $this->hasOne(Student::class, 'account_id');
+    }
+
+    public function lecturer()
+    {
+        return $this->hasOne(Lecturer::class, 'account_id');
+    }
+
+    public function admin()
+    {
+        return $this->hasOne(Admin::class, 'account_id');
+    }
 }
